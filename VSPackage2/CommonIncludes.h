e48e0716949436b3e96d9ff687a23e2a967ad603
James Hill <55800126+AnonaWolf@users.noreply.github.com>
2019-11-08 14:37:10 -0600

    Add project files.

---
+++ VSPackage2/CommonIncludes.h
@@ -0,0 +1,22 @@
+﻿// CommonIncludes.h
+
+#pragma once
+
+#include "Resource.h"
+#include "..\VSPackage2UI\Resource.h"
+#include "Guids.h"
+#include "..\VSPackage2UI\CommandIds.h"
+
+#include "VSPackage2.h"
+
+#include "Package.h"
+
+
+#include "EditorDocument.h"
+#include "EditorAutomation.inl"
+#include "EditorCore.inl"
+#include "EditorEvents.inl"
+#include "EditorFindAndReplace.inl"
+#include "EditorPersistance.inl"
+#include "EditorWindowing.inl"
+
